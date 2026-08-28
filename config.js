d9746c526a94588340f0b0d644c2f6ce456f2a06
dpanasiuk1207-sys <dpanasiuk1207@icloud.com>
2026-08-28 16:31:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.VT_CONFIG = {
-  ORDER_ENDPOINT: "https://script.google.com/macros/s/AKfycbwxt6hLajR377O-Zjl5tBl8a2Jczo5-U3XRPdNbOrNiwKz1SlHgKpE5vVQsgVN0OTJBrw/exec",
+  ORDER_ENDPOINT: "https://script.google.com/macros/s/AKfycbwVqIPRws7I9E1iIw2mJLkqNk3ATKbJKzuuhRZQ4r5Ilbed6b9gofto5ipTm-A4GBJY/exec",
   INSTAGRAM: "https://www.instagram.com/your_textiless/",
   SITE_URL: "https://dpanasiuk1207-sys.github.io/vash-textil",
   PAYMENT_MODE: "test",
